91fb824a5cc42222e067d2f718f941e6c5ed9eb2
Hannes Braun <hannesbraun@mail.de>
2021-03-10 14:36:15 +0100

    Make knob's areas more narrow

--- BitReKt.cpp
+++ BitReKt.cpp
@@ -25,6 +25,7 @@ BitReKt::BitReKt(const InstanceInfo& info)
     const IRECT b = pGraphics->GetBounds();
     pGraphics->AttachControl(new ITextControl(b.GetFromTop(100).GetCentredInside(PLUG_WIDTH, 60), "BitReKt", IText(50)));
 
+    // Status LEDs
     pGraphics->AttachControl(new ILEDControl(b.GetReducedFromTop(90).GetFromTop(14).GetReducedFromRight(20).GetFromRight(20), COLOR_GREEN), kCtrlTagSignalIn);
     pGraphics->AttachControl(new ITextControl(b.GetReducedFromTop(90).GetFromTop(14).GetReducedFromRight(45).GetFromRight(39), "Signal In", IText(14)));
     pGraphics->AttachControl(new ILEDControl(b.GetReducedFromTop(110).GetFromTop(14).GetReducedFromRight(20).GetFromRight(20), COLOR_BLUE), kCtrlTagOfflineRender);
@@ -83,12 +84,14 @@ BitReKt::BitReKt(const InstanceInfo& info)
       }, // Colors
     };
 
-    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(170).GetFromTop(100).GetFromLeft(PLUG_WIDTH / 2), kBits, "", redKnobStyle));
-    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(170).GetFromTop(100).GetFromRight(PLUG_WIDTH / 2), kSampleRate, "", blueKnobStyle));
-    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(300).GetFromTop(100).GetFromLeft(PLUG_WIDTH / 3), kInputGain, "", blackKnobStyle));
-    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(300).GetFromTop(100).GetReducedFromLeft(PLUG_WIDTH / 3).GetReducedFromRight(PLUG_WIDTH / 3), kMix, "", blackKnobStyle));
-    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(300).GetFromTop(100).GetFromRight(PLUG_WIDTH / 3), kOutputGain, "", blackKnobStyle));
+    // Knobs
+    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(170).GetFromTop(100).GetFromLeft(PLUG_WIDTH / 2).GetReducedFromLeft(PLUG_WIDTH / 6).GetCentredInside(100), kBits, "", redKnobStyle));
+    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(170).GetFromTop(100).GetFromRight(PLUG_WIDTH / 2).GetReducedFromRight(PLUG_WIDTH / 6).GetCentredInside(100), kSampleRate, "", blueKnobStyle));
+    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(300).GetFromTop(100).GetFromLeft(PLUG_WIDTH / 3).GetCentredInside(100), kInputGain, "", blackKnobStyle));
+    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(300).GetFromTop(100).GetReducedFromLeft(PLUG_WIDTH / 3).GetReducedFromRight(PLUG_WIDTH / 3).GetCentredInside(100), kMix, "", blackKnobStyle));
+    pGraphics->AttachControl(new IVKnobControl(b.GetReducedFromTop(300).GetFromTop(100).GetFromRight(PLUG_WIDTH / 3).GetCentredInside(100), kOutputGain, "", blackKnobStyle));
 
+    // Footnote thing
     pGraphics->AttachControl(new ITextToggleControl(b.GetReducedFromTop(430).GetFromTop(20).GetFromLeft(PLUG_WIDTH / 3).GetCentredInside(PLUG_WIDTH / 4), kClippingEnabled, "Clipping disabled", "Clipping enabled"));
     pGraphics->AttachControl(new ITextControl(b.GetReducedFromTop(430).GetFromTop(20).GetCentredInside(PLUG_WIDTH / 2), "Copyright 2021 Hannes Braun", IText(14)));
     pGraphics->AttachControl(new ITextControl(b.GetReducedFromTop(430).GetFromTop(20).GetFromRight(PLUG_WIDTH / 3), "Version 1.0.0", IText(14)));
@@ -97,12 +100,12 @@ BitReKt::BitReKt(const InstanceInfo& info)
 
 void BitReKt::OnIdle()
 {
-  // Status LEDs
+  // Controlling status LEDs
   if (!GetBypassed())
     SendControlValueFromDelegate(kCtrlTagSignalIn, 1.0);
   else
     SendControlValueFromDelegate(kCtrlTagSignalIn, 0.0);
-    
+
   if (GetRenderingOffline())
     SendControlValueFromDelegate(kCtrlTagOfflineRender, 1.0);
   else
